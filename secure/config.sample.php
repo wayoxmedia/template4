af979e9f141f1c00adf9b32e8d14c1f2ac9c4b1e
Eduardo Sanchez <walkovik@gmail.com>
2025-07-18 14:31:29 -0400

    Added Initial Files

---
+++ secure/config.sample.php
@@ -0,0 +1,10 @@
+<?php
+
+const SITE_NAME = 'Template 4';
+const SITE_CODE_NAME = 'template4';
+const SITE_URL = 'http://template4.test/';
+const SITE_ADMIN_URL = 'http://template4.test/admin/';
+const SUPPORT_EMAIL = 'mail@email.com';
+const API_URL = 'http://route.test/api';
+const VERSION = '1.0.0';
+
